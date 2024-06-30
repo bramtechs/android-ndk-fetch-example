84cdb31801c0746de2af3c46678f8e4602bc8a36
bramtechs <bramtech@telenet.be>
2024-06-30 18:21:14 +0200

    refresh button, counter example

--- app/src/main/cpp/fetch.cpp
+++ app/src/main/cpp/fetch.cpp
@@ -3,8 +3,11 @@
 
 #define BRIDGE_METHOD(X) Java_com_doomhowl_fetchexample_FetchExampleActivity_##X
 
+static int counter = 0;
+
 extern "C" JNIEXPORT jstring JNICALL BRIDGE_METHOD(stringFromJNI)(JNIEnv* env,
-                                                 jobject /* this */) {
-  std::string hello = "Hello world!";
+                                                 jobject /* this */)
+{
+  std::string hello = "Hello world! " + std::to_string(counter++);
   return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/kotlin/com/doomhowl/fetchexample/FetchExampleActivity.kt
+++ app/src/main/kotlin/com/doomhowl/fetchexample/FetchExampleActivity.kt
@@ -1,17 +1,30 @@
 package com.doomhowl.fetchexample
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.doomhowl.fetchexample.databinding.ActivityExampleJniBinding;
 
 class FetchExampleActivity : AppCompatActivity() {
 
+    private var binding: ActivityExampleJniBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityExampleJniBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.helloTextview.text = stringFromJNI()
+        binding = ActivityExampleJniBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        refreshText()
+    }
+
+    private fun refreshText()  {
+        binding?.let { binding ->
+            binding.helloTextview.text = stringFromJNI()
+        }
+    }
+
+    fun onRefreshButtonClicked(view: View) {
+        refreshText()
     }
 
     external fun stringFromJNI(): String?
